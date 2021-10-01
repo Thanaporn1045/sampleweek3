45cb735513bccfe4d270ba28def233ed28642ebb
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2021-10-01 20:53:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace week1
+{
+    class Unitconverter
+    {
+        int ratio;
+
+        public Unitconverter (int Unitratio)
+        { ratio = Unitratio; }
+        public int Convert (int Unit)
+        { return Unit * ratio; }
+
+
+    }
+    class Panda
+    {
+        public string name;
+        private int poppulation;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unitconverter feettoInchesConverter = new Unitconverter(12);
+            Unitconverter miletoInchesConverter = new Unitconverter(5200);
+
+            Console.WriteLine(feettoInchesConverter.Convert(30));
+            Console.WriteLine(feettoInchesConverter.Convert(100));
+            Console.WriteLine(feettoInchesConverter.Convert(miletoInchesConverter.Convert(1)));
+
+
+
+        }
+    }
+}
